5792a2c29d70510d2abaf58fe57921b733f9306c
ashutosh229 <akumarjha875@gmail.com>
2025-01-27 19:06:52 +0530

    implemented error handler service

---
+++ server/server/src/common/errors/errors.service.ts
@@ -0,0 +1,19 @@
+import { Logger } from '@nestjs/common';
+
+export class ErrorHandlerService {
+  private readonly logger = new Logger(ErrorHandlerService.name);
+
+  handleError(error: Error) {
+    if (
+      error.message.includes('Third argument must be an integer') ||
+      error.message.includes('SCardGetStatusChange') ||
+      error.message.includes(
+        'The specified reader is not currently available for use',
+      )
+    ) {
+      this.logger.error('Detected error condition, restarting application');
+    } else {
+      this.logger.error('error here');
+    }
+  }
+}
