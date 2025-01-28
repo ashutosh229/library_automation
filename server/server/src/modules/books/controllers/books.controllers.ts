600181a5adb342e14e2567465e3229c6ca1b6d60
ashutosh229 <akumarjha875@gmail.com>
2025-01-29 00:53:52 +0530

    added the controllers for the books

---
+++ server/server/src/modules/books/controllers/books.controllers.ts
@@ -0,0 +1,42 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { BooksService } from '../services/books.service';
+import { CreateBookDto } from '../dto/createBook.dto';
+import { UpdateBookDto } from '../dto/updateBook.dto';
+
+@Controller('books')
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+
+  @Post()
+  create(@Body() createBookDto: CreateBookDto) {
+    return this.booksService.create(createBookDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.booksService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.booksService.findOne(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateBookDto: UpdateBookDto) {
+    return this.booksService.update(id, updateBookDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.booksService.remove(id);
+  }
+}
