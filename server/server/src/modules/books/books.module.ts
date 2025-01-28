e675a152676108267e5b4c3456714bbc8b186407
ashutosh229 <akumarjha875@gmail.com>
2025-01-29 00:54:48 +0530

    added the module for the books

--- server/server/src/modules/books/books.module.ts
+++ server/server/src/modules/books/books.module.ts
@@ -1,1 +1,14 @@
- 
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Book, BookSchema } from './entities/books.schema';
+import { BooksService } from './services/books.service';
+import { BooksController } from './controllers/books.controllers';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }]),
+  ],
+  controllers: [BooksController],
+  providers: [BooksService],
+})
+export class BooksModule {}
