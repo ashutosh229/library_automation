6725530c000699220d8b7f9a7bc7e788a718e658
ashutosh229 <akumarjha875@gmail.com>
2025-01-28 22:27:43 +0530

    added the user module

--- server/server/src/modules/users/users.module.ts
+++ server/server/src/modules/users/users.module.ts
@@ -1,1 +1,14 @@
- 
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './entities/users.schema';
+import { UsersService } from './services/users.service';
+import { UsersController } from './controllers/users.controllers';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
