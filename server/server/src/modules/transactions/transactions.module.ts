287d7e4cc8393c276641f274a49a6adeceeefbf9
ashutosh229 <akumarjha875@gmail.com>
2025-01-29 00:49:32 +0530

    added the module for the transactions

--- server/server/src/modules/transactions/transactions.module.ts
+++ server/server/src/modules/transactions/transactions.module.ts
@@ -1,1 +1,17 @@
- 
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Transaction, TransactionSchema } from './entities/transactions.schema';
+import { TransactionService } from './services/transaction.service';
+import { TransactionController } from './controllers/transactions.controllers';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Transaction.name, schema: TransactionSchema },
+    ]),
+  ],
+  controllers: [TransactionController],
+  providers: [TransactionService],
+  exports: [TransactionService],
+})
+export class TransactionsModule {}
