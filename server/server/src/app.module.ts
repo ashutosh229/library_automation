67cab6e1d5c6f4b576f8ff7c8afb8a206e701eec
ashutosh229 <akumarjha875@gmail.com>
2025-01-29 14:52:18 +0530

    added the environment variables and also added the app module

--- server/server/src/app.module.ts
+++ server/server/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './modules/users/users.module';
+import { BooksModule } from './modules/books/books.module';
+import { TransactionsModule } from './modules/transactions/transactions.module';
+import { AttendanceModule } from './modules/attendance/attendance.module';
+import { AppController } from './app.controller'; // Import AppController
+import { AppService } from './app.service'; // Import AppService
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot(process.env.MONGO_URL),
+    UsersModule,
+    BooksModule,
+    TransactionsModule,
+    AttendanceModule,
+  ],
+  controllers: [AppController], // Add AppController
+  providers: [AppService], // Add AppService
 })
 export class AppModule {}
