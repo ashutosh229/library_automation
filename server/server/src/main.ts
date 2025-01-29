9cba5caef64954f3c54c672acb3919de4692dfa9
ashutosh229 <akumarjha875@gmail.com>
2025-01-29 14:53:07 +0530

    configured the main entry point of the application

--- server/server/src/main.ts
+++ server/server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Configure CORS
+  const corsOptions: CorsOptions = {
+    origin: 'http://localhost:5000', // Replace with your frontend URL during development
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+
+  app.enableCors(corsOptions);
+
+  await app.listen(3000);
+  // console.log(`Application is running on: ${await app.getUrl()}`);
+  // console.log(app.getHttpServer()._events.request._router.stack);
 }
 bootstrap();
