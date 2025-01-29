25693c490d5eec35f275bac431d9e5124955ecc9
ashutosh229 <akumarjha875@gmail.com>
2025-01-29 14:39:27 +0530

    added the module for the auth

--- server/server/src/modules/auth/auth.module.ts
+++ server/server/src/modules/auth/auth.module.ts
@@ -1,1 +1,23 @@
- 
+// import { Module } from '@nestjs/common';
+// import { JwtModule } from '@nestjs/jwt';
+// import { PassportModule } from '@nestjs/passport';
+// import { AuthService } from './services/auth.service';
+// import { AuthController } from './controllers/auth.controller';
+// import { LocalStrategy } from './strategies/local.strategy';
+// import { JwtStrategy } from './strategies/jwt.strategy';
+// import { UsersModule } from '../users/users.module';
+// import { jwtConstants } from './constants';
+
+// @Module({
+//   imports: [
+//     UsersModule,
+//     PassportModule,
+//     JwtModule.register({
+//       secret: jwtConstants.secret,
+//       signOptions: { expiresIn: '60m' },
+//     }),
+//   ],
+//   controllers: [AuthController],
+//   providers: [AuthService, LocalStrategy, JwtStrategy],
+// })
+// export class AuthModule {}
