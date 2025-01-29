b69bea3bef0f88a2b14fc7d5be41a2335894aee5
ashutosh229 <akumarjha875@gmail.com>
2025-01-29 14:49:18 +0530

    added the module for the attendance

--- server/server/src/modules/attendance/attendance.module.ts
+++ server/server/src/modules/attendance/attendance.module.ts
@@ -1,1 +1,21 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AttendanceService } from '../attendance/services/attendance.services';
+import { Attendance, AttendanceSchema } from './entities/attendance.schema';
+import { NFCService } from 'src/hardware/nfc/nfc.service';
+import { AttendanceController } from '../attendance/controllers/attendance.controllers';
+// import { TransactionService } from 'src/modules/transactions/services/transactions.service';
+import { TransactionsModule } from '../transactions/transactions.module';
 
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Attendance.name, schema: AttendanceSchema },
+    ]),
+    TransactionsModule,
+  ],
+  controllers: [AttendanceController],
+  providers: [AttendanceService, NFCService],
+  exports: [AttendanceService],
+})
+export class AttendanceModule {}
